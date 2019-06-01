0cdf6d7bc8722af157ef6aa353ec4634023c2ffd
issa <d0zer807@gmail.com>
2019-06-01 13:13:33 +0300

    added line between points, part 1,2 done

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -12,15 +12,17 @@ namespace Ex3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("pathOnMap", "display/{ip}/{port}/{rate}",
+               defaults: new { controller = "First", action = "pathOnMap" });
+
             routes.MapRoute("display", "display/{ip}/{port}",
                 defaults: new { controller = "First", action = "display" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "FirstController" + "", action = "Index", id = UrlParameter.Optional }
              );
-            routes.MapRoute("displayPath", "display/{ip}/{port}/{rate}",
-                new {controller = "First", action = "pathOnMap" });
             /*
             routes.MapRoute("displayPath", "/save/{ip}/{port}/4/10/flight1",
                 new { controller = "First", action = "display" });
